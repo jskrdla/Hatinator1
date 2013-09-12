038c4346af4dd734f39859419ed78ebf5c0bbc22
jskrdla <jskrdla@gmail.com>
2013-09-11 22:55:03 -0500

    initial load

---
+++ Hatinator/Hatinator/frmMain.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLottery = New System.Windows.Forms.Button()
+        Me.btnRandom = New System.Windows.Forms.Button()
+        Me.lbHat = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbResults = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnLottery
+        '
+        Me.btnLottery.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLottery.Location = New System.Drawing.Point(312, 8)
+        Me.btnLottery.Name = "btnLottery"
+        Me.btnLottery.Size = New System.Drawing.Size(88, 32)
+        Me.btnLottery.TabIndex = 0
+        Me.btnLottery.Text = "Lottery"
+        Me.btnLottery.UseVisualStyleBackColor = True
+        '
+        'btnRandom
+        '
+        Me.btnRandom.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRandom.Location = New System.Drawing.Point(408, 8)
+        Me.btnRandom.Name = "btnRandom"
+        Me.btnRandom.Size = New System.Drawing.Size(104, 32)
+        Me.btnRandom.TabIndex = 2
+        Me.btnRandom.Text = "Random"
+        Me.btnRandom.UseVisualStyleBackColor = True
+        '
+        'lbHat
+        '
+        Me.lbHat.FormattingEnabled = True
+        Me.lbHat.Location = New System.Drawing.Point(8, 88)
+        Me.lbHat.Name = "lbHat"
+        Me.lbHat.Size = New System.Drawing.Size(296, 251)
+        Me.lbHat.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 72)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Names in hat:"
+        '
+        'lbResults
+        '
+        Me.lbResults.FormattingEnabled = True
+        Me.lbResults.Location = New System.Drawing.Point(312, 88)
+        Me.lbResults.Name = "lbResults"
+        Me.lbResults.Size = New System.Drawing.Size(344, 251)
+        Me.lbResults.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(312, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Results:"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.ForeColor = System.Drawing.Color.ForestGreen
+        Me.btnAdd.Location = New System.Drawing.Point(8, 8)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(32, 32)
+        Me.btnAdd.TabIndex = 7
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRemove.ForeColor = System.Drawing.Color.DarkRed
+        Me.btnRemove.Location = New System.Drawing.Point(40, 8)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(32, 32)
+        Me.btnRemove.TabIndex = 8
+        Me.btnRemove.Text = "-"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(80, 8)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(224, 32)
+        Me.txtName.TabIndex = 9
+        '
+        'frmMain
+        '
+        Me.AcceptButton = Me.btnAdd
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(664, 365)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lbResults)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbHat)
+        Me.Controls.Add(Me.btnRandom)
+        Me.Controls.Add(Me.btnLottery)
+        Me.Name = "frmMain"
+        Me.Text = "Hatinator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnLottery As System.Windows.Forms.Button
+    Friend WithEvents btnRandom As System.Windows.Forms.Button
+    Friend WithEvents lbHat As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lbResults As System.Windows.Forms.ListBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnRemove As System.Windows.Forms.Button
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+
+End Class
---
+++ Hatinator/Hatinator/frmMain.vb
@@ -0,0 +1,174 @@
+﻿Imports System.Security.Cryptography
+
+Public Class frmMain
+
+
+    Private Sub btnLottery_Click(sender As Object, e As EventArgs) Handles btnLottery.Click
+        lbResults.Items.Clear()
+        Dim names As New List(Of String)
+        names = GetNames()
+        Dim hat As New List(Of String)
+        Dim iCount As Integer = names.Count
+        For Each item As String In names
+            For x As Integer = 1 To iCount
+                hat.Add(item)
+            Next
+            iCount = iCount - 1
+        Next
+        While hat.Count > 0
+            lbResults.Items.Add(Draw(hat))
+        End While
+    End Sub
+
+    Private Function Draw(names As List(Of String)) As String
+        Dim sName As String = ""
+        Try
+            If names.Count > 1 Then
+                names.Sort(New Randomizer(Of String)())
+                GenerateRandomNumbers(names.Count)
+                Dim index As Integer = CInt(pickRandomNumberInRange(0, names.Count - 1))
+                If index < names.Count - 1 Then
+                    sName = names(index)
+                End If
+            Else
+                sName = names(0)
+            End If
+
+            While names.Contains(sName)
+                names.Remove(sName)
+            End While
+        Catch ex As Exception
+            sName = Draw(names)
+        End Try
+
+        Return sName
+
+    End Function
+
+    Private RandomNumbers As New List(Of Decimal)
+
+    Private Sub GenerateRandomNumbers(ByVal cnt As Integer)
+        While RandomNumbers.Count < cnt
+            Dim num As Decimal = GetRandomNumber()
+            If Not RandomNumbers.Contains(num) Then RandomNumbers.Add(num)
+        End While
+    End Sub
+
+    Private Function pickRandomNumberInRange(min As Integer, max As Integer) As Integer
+        Dim rand As Integer = PickRandomNumber()
+        If rand >= min And rand <= max Then
+            Return rand
+        ElseIf rand < min Then
+            Return min
+        ElseIf rand > max Then
+            Return rand Mod max
+        End If
+        Return rand
+    End Function
+
+    Private Function PickRandomNumber() As Decimal
+        If RandomNumbers.Count < 100 Then GenerateRandomNumbers(1000)
+        Dim index As Integer = CInt(GetRandomNumber(RandomNumbers.Count))
+        While index > RandomNumbers.Count - 1
+            index = GetRandomNumber(RandomNumbers.Count)
+        End While
+        Dim retVal As Decimal = RandomNumbers(index)
+        RandomNumbers.RemoveAt(index)
+        Return retVal
+    End Function
+
+    Private Function GetRandomNumber(Optional MaxValue As Decimal = 1000000) As Decimal
+        If MaxValue < 1 Then MaxValue = 1
+        Randomize()
+        Dim rnd = New Random()
+        Dim nextValue As Decimal = rnd.Next(1001) * DateTime.Now.Millisecond
+        Return nextValue Mod MaxValue
+    End Function
+
+    Private Function GetNames() As List(Of String)
+        Dim names As New List(Of String)
+        For Each item As String In lbHat.Items
+            If names.Contains(item) Then
+                MessageBox.Show("Can't use the same name twice.")
+                Return New List(Of String)
+            Else
+                names.Add(item)
+            End If
+        Next
+        Return names
+    End Function
+
+    Private Sub btnRandom_Click(sender As Object, e As EventArgs) Handles btnRandom.Click
+        lbResults.Items.Clear()
+        Dim names As New List(Of String)
+        names = GetNames()
+        While names.Count > 0
+            lbResults.Items.Add(Draw(names))
+        End While
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        If lbHat.SelectedIndex > -1 Then
+            lbHat.Items.RemoveAt(lbHat.SelectedIndex)
+            If lbHat.Items.Count > 0 Then
+                lbHat.SelectedIndex = lbHat.Items.Count - 1
+            End If
+            SetHatCount()
+        End If
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If txtName.Text.Length > 0 Then
+            Dim sItems As String() = txtName.Text.Split(",")
+            For Each s As String In sItems
+                lbHat.Items.Add(Trim(s))
+            Next
+            lbHat.SelectedIndex = lbHat.Items.Count - 1
+            txtName.Text = ""
+            txtName.Focus()
+            SetHatCount()
+        End If
+    End Sub
+
+    Public Class Randomizer(Of T)
+        Implements IComparer(Of T)
+
+        ''// Ensures different instances are sorted in different orders
+        Private Shared Salter As New Random() ''// only as random as your seed
+        Private Salt As Integer
+        Public Sub New()
+            Salt = Salter.Next(Integer.MinValue, Integer.MaxValue)
+        End Sub
+
+        Private Shared sha As New SHA1CryptoServiceProvider()
+        Private Function HashNSalt(ByVal x As Integer) As Integer
+            Dim b() As Byte = sha.ComputeHash(BitConverter.GetBytes(x))
+            Dim r As Integer = 0
+            For i As Integer = 0 To b.Length - 1 Step 4
+                r = r Xor BitConverter.ToInt32(b, i)
+            Next
+
+            Return r Xor Salt
+        End Function
+
+        Public Function Compare(x As T, y As T) As Integer _
+            Implements IComparer(Of T).Compare
+
+            Return HashNSalt(x.GetHashCode()).CompareTo(HashNSalt(y.GetHashCode()))
+        End Function
+    End Class
+
+    Private Sub frmMain_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Delete Then
+            btnRemove.PerformClick()
+        End If
+    End Sub
+
+    Private Sub SetHatCount()
+        If lbHat.Items.Count = 0 Then
+            Label1.Text = "Names in hat:"
+        Else
+            Label1.Text = "Names in hat (" & lbHat.Items.Count & "):"
+        End If
+    End Sub
+End Class
